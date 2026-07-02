1a8a228de76b31af10ff045607c9909907525b50
Shifeng Hong <sfHong512@gmail.com>
2026-07-02 11:04:07 +0800

    Update to syntatic html element tag.

--- src/lib/components/UserCard.svelte
+++ src/lib/components/UserCard.svelte
@@ -15,22 +15,21 @@
   const roleLabel = $derived(user.role === "group-admin" ? "管理员" : "成员");
 </script>
 
-<div
+<article
   class="group flex items-center rounded-full p-2 gap-2.5 transition-all hover:-translate-y-1 hover:shadow-sm border border-dashed border-neutral-300 admin:border-solid admin:border-neutral-500 w-60"
   class:admin={user.role === "group-admin"}
 >
-  <span
-    class="avatar font-medium text-xl text-black"
+  <span class="avatar font-medium text-xl text-black"
     >{user.nick_name?.at(0) ?? "?"}</span
   >
 
-  <div class="center flex-col gap-0.5">
-    <span class="font-light">{user.nick_name ?? "未知"}</span>
+  <hgroup class="center flex-col gap-0.5">
+    <h4 class="font-light">{user.nick_name ?? "未知"}</h4>
     <span class="text-xs bg-neutral-200 px-1 py-0 rounded-sm">{roleLabel}</span>
-  </div>
+  </hgroup>
 
   <span
     class="text-neutral-300 text-sm ml-auto mr-2 group-hover:text-neutral-400"
     >{user.account}</span
   >
-</div>
+</article>
